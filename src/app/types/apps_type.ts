481e006c3e5aade8b3981d5c6b4ccafe81a3bdd4
Raphael Wirth <wirtr3@bfh.ch>
2024-09-20 09:01:42 +0200

    First commit

---
+++ src/app/types/apps_type.ts
@@ -0,0 +1,11 @@
+export interface AppType {
+    id: string
+    name: string
+    image: string
+    link: string
+    tab: string
+    caption: string
+    isFull: boolean
+    primary?: string
+    background: string
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://us-central1-junkielabs-57977.cloudfunctions.net/apiPortfolio',
+  recaptcha: '6Lci95YiAAAAAOfkpaWwNSHiX5Ye7KT9ZOw-HYCv'
+};
