62ad68f0f5196a09b1bbc3f91c7dfaebb56ff1c6
Raphael Wirth <wirtr3@bfh.ch>
2024-09-20 13:52:00 +0200

    zurücksetzen

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-  production: false,
-  apiUrl: 'https://us-central1-junkielabs-57977.cloudfunctions.net/apiPortfolio',
-  recaptcha: '6Lci95YiAAAAAOfkpaWwNSHiX5Ye7KT9ZOw-HYCv'
-};
