a1965df4fe1ac616f7f11ab00b7940794ed1111d
Raphael Wirth <wirtr3@bfh.ch>
2024-10-04 12:40:21 +0200

    Animationen hinzugefügt, funktioniert aber noch nicht

--- src/app/Hauptbody/uebermich/uebermich.component.ts
+++ src/app/Hauptbody/uebermich/uebermich.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-uebermich',
   standalone: true,
-  imports: [],
   templateUrl: './uebermich.component.html',
-  styleUrl: './uebermich.component.scss'
+  styleUrls: ['./uebermich.component.scss']
 })
-export class UebermichComponent {
+export class UebermichComponent implements OnInit {
 
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+        } else {
+          entry.target.classList.remove('in-view');
+        }
+      });
+    });
+
+    const elements = this.el.nativeElement.querySelectorAll('.slide-in-element');
+    elements.forEach((el: Element) => observer.observe(el));
+  }
 }
